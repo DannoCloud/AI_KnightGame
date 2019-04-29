c2fbccf6e81c7ea86a071b8e11ad8e43b1500e0e
Daniel Collins <daisuke31@sbcglobal.net>
2019-04-29 15:22:33 -0500

    newest update

---
+++ AI_Final/Assets/Scripts/PlayerDeath.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class PlayerDeath : MonoBehaviour
+{
+    
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "Enemy_AI")
+        {
+            Destroy(col.gameObject);
+        } 
+
+        if (col.gameObject.name == "Player_knight")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
